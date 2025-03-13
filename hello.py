ca989dbcfeb6747b4de3bc8976f7bb87cd5fa7ba
Endaline <endalineihuoma7@gmail.com>
2025-03-13 16:30:05 +0100

    my first code on python

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("My first python Code please Lord see me through in this journey")
+print("I must make it")
+print("Trump regim must favour me Amen")
+print(4+9)
+name ="Endaline"
+
+# If a jet fle the USA to Uk, distance 6484km in 9hours. What was the speed of the jet? speed = distance/time
+
+distance = 6484
+time = 9
+speed = distance/time
+print("The jet was flying at speed of", speed, "Km/hr")
+
+print(type(distance))
+print(type(name))
+
+# How to generate random numbers in python
+
