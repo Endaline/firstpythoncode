06f9d95f7f36444cd2fccf0ef3cb77f11191ad98
Endaline <endalineihuoma7@gmail.com>
2025-04-28 20:09:13 +0100

    static method in python

---
+++ property_decoretor.py
@@ -0,0 +1,4 @@
+# @property = This is a decorator that allows you to define a method as a property.
+# It allows you to access a method as if it were an attribute, without needing to call it like a function.
+# This is useful for encapsulating data and providing a clean interface for accessing attributes.
+# Gives you setter , getter and deleter methods
